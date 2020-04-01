d4198e1c99bcc6a872f8e2728b7f1a0475663aa5
mynameisken <53652743+kenismyname@users.noreply.github.com>
2020-04-01 10:17:57 +0800

    UWP OCR in Winform
    Made by mynameisken (高剑 [gau dʒiæn] in Chinese)
    Inter-Operation between Winfrom and UWP is not an easy job.
    This program bridges them together, so Winfrom can share UWP power.
    
    ----------------------------------------------------------------------------------------
    Start with installation (already done in this project):
    https://docs.microsoft.com/zh-cn/windows/apps/desktop/modernize/desktop-to-uwp-enhance
    
    Efforts in this work:
    (1) .Net Winform program can invoke UWP packages.
    (2) UWP OCR function as sample.
    (3) Change SoftwareBitmap to Bitmap and Bitmap to SoftwareBitma as well.
    ----------------------------------------------------------------------------------------
    
    :)

---
+++ Winform_Interop_UWP_OCR/FormMain.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Winform_Interop_UWP_OCR
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.boxClip = new System.Windows.Forms.PictureBox();
+            this.boxOCR = new System.Windows.Forms.PictureBox();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            ((System.ComponentModel.ISupportInitialize)(this.boxClip)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxOCR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // boxClip
+            // 
+            this.boxClip.Dock = System.Windows.Forms.DockStyle.Left;
+            this.boxClip.Location = new System.Drawing.Point(0, 0);
+            this.boxClip.Margin = new System.Windows.Forms.Padding(0);
+            this.boxClip.Name = "boxClip";
+            this.boxClip.Size = new System.Drawing.Size(426, 345);
+            this.boxClip.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.boxClip.TabIndex = 1;
+            this.boxClip.TabStop = false;
+            this.boxClip.Click += new System.EventHandler(this.boxClip_Click);
+            // 
+            // boxOCR
+            // 
+            this.boxOCR.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.boxOCR.Location = new System.Drawing.Point(426, 0);
+            this.boxOCR.Margin = new System.Windows.Forms.Padding(0);
+            this.boxOCR.Name = "boxOCR";
+            this.boxOCR.Size = new System.Drawing.Size(404, 345);
+            this.boxOCR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.boxOCR.TabIndex = 2;
+            this.boxOCR.TabStop = false;
+            this.boxOCR.Click += new System.EventHandler(this.boxOCR_Click);
+            // 
+            // splitter1
+            // 
+            this.splitter1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitter1.Location = new System.Drawing.Point(426, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(6, 345);
+            this.splitter1.TabIndex = 3;
+            this.splitter1.TabStop = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 345);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.boxOCR);
+            this.Controls.Add(this.boxClip);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "TestBot V1.0";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.SizeChanged += new System.EventHandler(this.FormMain_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.boxClip)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxOCR)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.PictureBox boxClip;
+        private System.Windows.Forms.PictureBox boxOCR;
+        private System.Windows.Forms.Splitter splitter1;
+    }
+}
+
---
+++ Winform_Interop_UWP_OCR/FormMain.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Windows.Media.Ocr;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
+using Windows.ApplicationModel.DataTransfer;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using Windows.Globalization;
+using Windows.System.UserProfile;
+
+namespace Winform_Interop_UWP_OCR
+{
+    // Made by mynameisken(高剑(Gao Jian) in Chinese)
+    // Inter-Operation between Winfrom and UWP is not an easy job.
+    // This program bridges them together, so Winfrom can share UWP power as well.
+    public partial class FormMain : Form
+    {
+        //Ocr engine from UWP sdk packages
+        private OcrEngine ocr;
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            SetLayout();
+
+            // Init OCR engine
+            var topUserLanguage = GlobalizationPreferences.Languages[0];
+            var language = new Language(topUserLanguage);
+            var displayName = language.DisplayName;
+            ocr = OcrEngine.TryCreateFromLanguage(language);
+
+            // Regiter Clipboard event handler
+            Windows.ApplicationModel.DataTransfer.Clipboard.Clear();
+            Windows.ApplicationModel.DataTransfer.Clipboard.ContentChanged += Clipboard_ContentChanged;
+        }
+
+        // Get image data in Clipboard
+        private async void Clipboard_ContentChanged(object sender, object e)
+        {
+            DataPackageView package = Windows.ApplicationModel.DataTransfer.Clipboard.GetContent();
+            if (package.Contains(StandardDataFormats.Bitmap))
+            {
+                RandomAccessStreamReference img = await package.GetBitmapAsync();
+                var imgstream = await img.OpenReadAsync();
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(imgstream);
+                SoftwareBitmap soft = await decoder.GetSoftwareBitmapAsync();
+                OcrResult result = await ocr.RecognizeAsync(soft);
+
+                Bitmap clip = Soft2Bitmap(soft);
+                boxClip.Image = clip;
+
+                Bitmap bmp = new Bitmap(clip.Width, clip.Height);
+                Graphics g = Graphics.FromImage(bmp);
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                foreach (var line in result.Lines)
+                {
+                    foreach (var word in line.Words)
+                    {
+                        Windows.Foundation.Rect r = word.BoundingRect;
+                        g.DrawRectangle(Pens.LightSkyBlue, (float)r.X, (float)r.Y, (float)r.Width, (float)r.Height);
+                        g.DrawString(word.Text, new Font("宋体", 9f), Brushes.Violet, (float)r.X, (float)r.Y);
+                    }
+                }
+                g.Dispose();
+
+                boxOCR.Image = bmp;
+            }
+        }
+        
+        // Convert UWP SoftwareBitmap to Bitmap
+        private unsafe Bitmap Soft2Bitmap(SoftwareBitmap soft)
+        {
+            int w = soft.PixelWidth;
+            int h = soft.PixelHeight;
+
+            //int stride = (w + 4 - (w % 4)) * 4;
+            int stride = w * 4;
+            int size = stride * h;
+            Windows.Storage.Streams.Buffer buf = new Windows.Storage.Streams.Buffer((uint)size);
+            soft.CopyToBuffer(buf);
+            byte[] data = new byte[size];
+            buf.CopyTo(data);
+
+            Bitmap bmp = new Bitmap(w, h);
+            BitmapData bdata = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.WriteOnly, bmp.PixelFormat);
+            IntPtr ptr = bdata.Scan0;
+            Marshal.Copy(data, 0, ptr, size);
+            bmp.UnlockBits(bdata);
+
+            return bmp;
+        }
+
+        // Convert Bitmap to UWP SoftwareBitmap
+        private unsafe SoftwareBitmap Bitmap2Soft(Bitmap bmp)
+        {
+            int w = bmp.Width;
+            int h = bmp.Height;
+            
+            BitmapData bdata = bmp.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.ReadOnly, bmp.PixelFormat);
+            IntPtr ptr = bdata.Scan0;
+            int size = bdata.Stride * bdata.Height;
+            byte[] data = new byte[size];
+            Marshal.Copy(ptr, data, 0, size);
+            bmp.UnlockBits(bdata);
+
+            IBuffer buf = data.AsBuffer();
+
+            SoftwareBitmap soft = new SoftwareBitmap(BitmapPixelFormat.Bgra8, w, h);
+            soft.CopyFromBuffer(buf);
+
+            return soft;
+        }
+
+        [ComImport]
+        [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        unsafe interface IMemoryBufferByteAccess
+        {
+            void GetBuffer(out byte* buffer, out uint capacity);
+        }
+
+        private unsafe void AccessPixel(SoftwareBitmap softwareBitmap)
+        {
+            using (BitmapBuffer buffer = softwareBitmap.LockBuffer(BitmapBufferAccessMode.Write))
+            {
+                using (var reference = buffer.CreateReference())
+                {
+                    byte* dataInBytes;
+                    uint capacity;
+                    ((IMemoryBufferByteAccess)reference).GetBuffer(out dataInBytes, out capacity);
+
+                    // Fill-in the BGRA plane
+                    BitmapPlaneDescription bufferLayout = buffer.GetPlaneDescription(0);
+                    for (int i = 0; i < bufferLayout.Height; i++)
+                    {
+                        for (int j = 0; j < bufferLayout.Width; j++)
+                        {
+
+                            byte value = (byte)((float)j / bufferLayout.Width * 255);
+                            dataInBytes[bufferLayout.StartIndex + bufferLayout.Stride * i + 4 * j + 0] = value;
+                            dataInBytes[bufferLayout.StartIndex + bufferLayout.Stride * i + 4 * j + 1] = value;
+                            dataInBytes[bufferLayout.StartIndex + bufferLayout.Stride * i + 4 * j + 2] = value;
+                            dataInBytes[bufferLayout.StartIndex + bufferLayout.Stride * i + 4 * j + 3] = (byte)255;
+                        }
+                    }
+                }
+            }
+        }
+
+        // Get a resized Bitmap
+        private Bitmap ResizeBitmap(Bitmap bmp, int w, int h)
+        {
+            Bitmap result = new Bitmap(w, h);
+            Graphics g = Graphics.FromImage(result);
+            g.DrawImage(bmp, 0, 0, w, h);
+            g.Dispose();
+            g = null;
+            return result;
+        }
+
+        private void FormMain_SizeChanged(object sender, EventArgs e)
+        {
+            SetLayout();
+        }
+
+        // Refresh Layout
+        private void SetLayout()
+        {
+            Rectangle rect = this.DisplayRectangle;
+
+            boxClip.Width = rect.Width / 2 - 5;
+        }
+
+        // Change PictureBox.SizeMode
+        private void boxClip_Click(object sender, EventArgs e)
+        {
+            if (boxClip.SizeMode == PictureBoxSizeMode.CenterImage)
+            {
+                boxClip.SizeMode = PictureBoxSizeMode.Zoom;
+                boxOCR.SizeMode = PictureBoxSizeMode.Zoom;
+                return;
+            }
+
+            if (boxClip.SizeMode == PictureBoxSizeMode.Zoom)
+            {
+                boxClip.SizeMode = PictureBoxSizeMode.CenterImage;
+                boxOCR.SizeMode = PictureBoxSizeMode.CenterImage;
+                return;
+            }
+        }
+
+        // Change PictureBox.SizeMode
+        private void boxOCR_Click(object sender, EventArgs e)
+        {
+            if (boxOCR.SizeMode == PictureBoxSizeMode.CenterImage)
+            {
+                boxClip.SizeMode = PictureBoxSizeMode.Zoom;
+                boxOCR.SizeMode = PictureBoxSizeMode.Zoom;
+                return;
+            }
+
+            if (boxOCR.SizeMode == PictureBoxSizeMode.Zoom)
+            {
+                boxClip.SizeMode = PictureBoxSizeMode.CenterImage;
+                boxOCR.SizeMode = PictureBoxSizeMode.CenterImage;
+                return;
+            }
+        }
+    }
+}
